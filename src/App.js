b1788680806be8c3b998141deb5639f704605467
zamorai <35697970+zamorai@users.noreply.github.com>
2020-09-19 22:16:42 -0400

    finished react-hooks

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import Accordion from './components/Accordion';
 import Dropdown from './components/Dropdown';
 import Search from './components/Search';
 import Translate from './components/Translate';
+import Route from './components/Route';
+import Header from './components/Header';
 
 const items = [
   {
@@ -37,11 +39,22 @@ const options = [
 const App = () => {
 
   const [selected, setSelected] = useState(options[0]);
-  const [showDropdown, setShowDropdown] = useState(true);
 
   return (
     <div>
-      <Translate />
+      <Header />
+      <Route path = "/">
+        <Accordion items = {items} />
+      </Route>
+      <Route path = "/list">
+        <Search />
+      </Route>
+      <Route path = "/dropdown">
+        <Dropdown label = "Select a colot" options = {options} selected= {selected} onSelectedChange = {setSelected}/>
+      </Route>
+      <Route path = "/translate">
+        <Translate />
+      </Route>
     </div>
   )
 }
